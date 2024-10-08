d9851146899fd4ae6e661f9b7a4eb3e4c7abf1a5
Ravi Shankar <ravishankar@outlook.in>
2024-10-08 06:08:00 +0530

    Initial Commit

---
+++ RSFrameworkV5/RSFrameworkV5.h
@@ -0,0 +1,18 @@
+//
+//  RSFrameworkV5.h
+//  RSFrameworkV5
+//
+//  Created by Ravi Shankar on 08/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSFrameworkV5.
+FOUNDATION_EXPORT double RSFrameworkV5VersionNumber;
+
+//! Project version string for RSFrameworkV5.
+FOUNDATION_EXPORT const unsigned char RSFrameworkV5VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSFrameworkV5/PublicHeader.h>
+
+
